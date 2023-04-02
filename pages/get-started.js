e34465613cc366b0141297085779d84ca578acc1
prashantp01 <prashant.paddune@cogoport.com>
2023-04-02 12:53:14 +0530

    initial commit
    Signed-off-by: prashantp01 <prashant.paddune@cogoport.com>

---
+++ pages/get-started.js
@@ -0,0 +1,37 @@
+import { Typography, Input, Button, Form } from 'antd';
+import styles from '../styles/home.module.css';
+const { Title } = Typography;
+import { SearchOutlined } from '@ant-design/icons';
+
+const handleSubmit = async (value) => {
+    const { query } = value;
+    const newVar = query.split(' ').join('-');
+
+    window.location.href = `/${newVar}`;
+}
+
+
+const GetStarted = () => {
+    return (
+        <div className={styles.container}>
+            <Form
+                name="basic"
+                style={{ display: 'flex', flexDirection: 'column', width: 300 }}
+                initialValues={{ remember: true }}
+                onFinish={handleSubmit}
+            >
+                <Title level={1}> I'm looking for </Title>
+                <section>
+                    <Form.Item rules={[{ required: true, message: 'Please input your query!' }]} name="query">
+                        <Input size="large" placeholder="eg: Musician portfolio website" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" style={{ marginTop:5 }} shape="circle" icon={<SearchOutlined />}/>
+                    </Form.Item>
+                </section>
+            </Form>
+        </div>
+    )
+}
+
+export default GetStarted;
